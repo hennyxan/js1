448e4731749a2b06c22eb42364c57115d295672d
Danyil Shchedrin <danyil.shchedrin@gmail.com>
2023-10-31 23:05:11 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber = 10;
+let secondNumber = 50;
+
+let sum = firstNumber + secondNumber;
+let difference = secondNumber - firstNumber;
+let product = firstNumber * secondNumber;
+let quotient = secondNumber / firstNumber;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
